91cbbe2d8ac25e08cf29bfd7e88ce6c5d01727ca
tolerious <tolerious@qq.com>
2016-11-30 14:24:40 +0800

    :construction: version 0.1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <tree-view></tree-view>
+    <tree-view :treeData="treeData"></tree-view>
   </div>
 </template>
 
 <script>
   import Hello from './components/Hello'
   import TreeView from './components/TreeView.vue'
+  let data = [
+    {
+      name: '第一章',
+      children: [
+        {
+          name: '第一章第一小节',
+          children: [
+            {
+              name: '第一章第一小节第一小段',
+              children: [
+                {
+                  name: '第一章第一小节第一小段第一小天地'
+                }
+              ]
+            },
+            {
+              name: '第一章第一小节第二小段'
+            }
+          ]
+        },
+        {
+          name: '第一章第二小节'
+        }
+      ]
+    },
+    {
+      name: '第二章',
+      children: [
+        {
+          name: '第二章第一小节'
+        }
+      ]
+    }
+  ]
 
   export default {
     name: 'app',
+    data () {
+      return {
+        treeData: data
+      }
+    },
     components: {
       Hello,
       TreeView
@@ -19,12 +58,4 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    abc
-    {{msg}}
+    <ul v-for="td in treeData">
+      <li>
+        <span>{{td.name}}</span>
+        <tree-view :treeData="td.children"></tree-view>
+      </li>
+    </ul>
   </div>
 </template>
 <style>
 </style>
 <script>
   export default{
+    name: 'tree-view',
+    props: ['treeData'],
     data () {
-      return {
-        msg: 'hello vue'
-      }
+      return {}
     },
     methods: {
       abc: function () {
