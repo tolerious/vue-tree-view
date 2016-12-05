66295f697588e0ddc9d19f153ae53358e92f5e59
tolerious <tolerious@qq.com>
2016-12-05 17:01:34 +0800

    :construction: update code.

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -3,12 +3,15 @@
     <ul v-for="td in treeData">
       <!--<span v-if="td.children.length>0" class="fa fa-sort-down">{{td.name}}</span>-->
       <!--<span v-else>{{td.name}}</span>-->
-      <span v-if="td.children.length>0" class="fa fa-sort-down" @click="handleCollapse(td)"></span>
+      <span v-if="td.children.length>0" class="fa fa-sort-down" @click="handleCollapseItem(td)"></span>
       <span v-else class="fa fa-sort-down" style="opacity: 0;"></span>
       <span>{{td.name}}</span>
-      <li v-show="isShow" :class="classObject">
-        <tree-view :treeData="td.children"></tree-view>
-      </li>
+      <span class="fa fa-trash-o" @click="handleDeleteItem"></span>
+      <span class="fa fa-plus" v-show="td.children.length>0"></span>
+
+      <!--<li v-show="isShow" :class="classObject">-->
+      <tree-view :treeData="td.children"></tree-view>
+      <!--</li>-->
     </ul>
   </div>
 </template>
@@ -26,6 +29,9 @@
     name: 'tree-view',
     props: ['treeData'],
     mounted: function () {
+      this.$nextTick(function () {
+        console.log('tick...')
+      })
     },
     data () {
       return {
@@ -48,10 +54,14 @@
       }
     },
     methods: {
-      handleCollapse: function (da) {
+      handleCollapseItem: function (da) {
         console.log('handle...')
         console.log(this.treeData.length)
         console.log(da.name)
+        console.log(da.children)
+      },
+      handleDeleteItem: function () {
+        console.log('delete item...')
       }
     },
     components: {}
