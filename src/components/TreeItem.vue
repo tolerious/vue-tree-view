b1e57d4cdb7f385c7ef5379542b2795ad9e20b2b
tolerious <tolerious@qq.com>
2016-12-06 17:37:30 +0800

    :construction: add trash icon.

--- src/components/TreeItem.vue
+++ src/components/TreeItem.vue
@@ -2,6 +2,7 @@
   <li>
     <span v-if="isFloder" class="fa fa-caret-down">{{data.name}}</span>
     <span v-else>{{data.name}}</span>
+    <span class="fa fa-trash" @click="handleDeleteItem"></span>
     <slot name="childTreeComponent"></slot>
   </li>
 </template>
@@ -18,6 +19,11 @@
     data () {
       return {}
     },
-    components: {}
+    components: {},
+    methods: {
+      handleDeleteItem: function () {
+        console.log('delete...')
+      }
+    }
   }
 </script>
